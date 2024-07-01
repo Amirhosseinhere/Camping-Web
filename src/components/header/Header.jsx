58308264631a0b0f37b9c1c2534903036b294b53
Stormsurge01 <156475637+Stormsurge01@users.noreply.github.com>
2024-07-02 01:01:17 +0330

    commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+function Header() {
+return (
+<>
+<header>
+    <div className="container">
+        <nav>
+            <a href="#" className="app-logo">
+                <img src="#" alt="Camping web" className="app-logo_img" />
+            </a>
+        </nav>
+        <ul className="menu">
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Home</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Distination</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Naer me</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Events</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Blog</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Gallry</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>About</a>
+            </li>
+
+            <li className="menu-item">
+                <a href="#" className='menu-link'>Contact us</a>
+            </li>
+        </ul>
+    </div>
+</header>
+</>
+)
+}
+
+export default Header
